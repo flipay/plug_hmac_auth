134e964e363e7c5ea960a930bc05eae3369bd658
Neo Ho <neofelisho@gmail.com>
2020-04-05 14:41:59 +0700

    Initial this project.

---
+++ lib/plug_hmac_auth.ex
@@ -0,0 +1,18 @@
+defmodule PlugHmacAuth do
+  @moduledoc """
+  Documentation for `PlugHmacAuth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugHmacAuth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_hmac_auth_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugHmacAuthTest do
+  use ExUnit.Case
+  doctest PlugHmacAuth
+
+  test "greets the world" do
+    assert PlugHmacAuth.hello() == :world
+  end
+end
